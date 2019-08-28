5a82a1cf56b487c27facffc8cc260ef0823d22f7
DinuwanB <dinubhagya97@gmail.com>
2019-08-29 02:02:34 +0530

    Updates

---
+++ precache-manifest.c25a02f27be6346cc02b588407d69cde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9517a80162bedf23eebd699c1b6c7772",
+    "url": "/Monsters-Rolodex-React/index.html"
+  },
+  {
+    "revision": "7db5ce2b9a8d169d9c50",
+    "url": "/Monsters-Rolodex-React/static/css/main.6bb3b7e9.chunk.css"
+  },
+  {
+    "revision": "f2a21588a85dc7437686",
+    "url": "/Monsters-Rolodex-React/static/js/2.f8a4d42e.chunk.js"
+  },
+  {
+    "revision": "7db5ce2b9a8d169d9c50",
+    "url": "/Monsters-Rolodex-React/static/js/main.d816fe3f.chunk.js"
+  },
+  {
+    "revision": "0120ed6ba0f2ca633494",
+    "url": "/Monsters-Rolodex-React/static/js/runtime~main.e2295e4b.js"
+  }
+]);
